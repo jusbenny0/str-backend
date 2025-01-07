1bee56c46cef48d3785682c5ddbd1baf85885858
jusbenny0 <stardestrojr@gmail.com>
2025-01-07 09:22:24 +0100

    removed uneccesary check for id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,9 +9,7 @@ export const test = (req, res) => {
 };
 
 export const updateUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id) {
-    return next(errorHandler(401, "You can update only your account!"));
-  }
+  
   try {
     if (req.body.password) {
       req.body.password = bcryptjs.hashSync(req.body.password, 10);
