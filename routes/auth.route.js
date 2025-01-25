18d0efc009cf2b9d16d95e1ba995a86bd8836e11
jusbenny0 <stardestrojr@gmail.com>
2025-01-25 22:30:14 +0100

    fixed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,5 +3,8 @@ import { signup, login, signout } from '../controllers/auth.controller.js';
 
 const router = express.Router()
 
+router.post('/signup', signup)
+router.post('/login', login)
+router.get('/signout', signout);
 
 export default router
