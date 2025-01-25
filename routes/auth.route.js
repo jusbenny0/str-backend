0978210c025d6f72c4d167defe2fd16eb002165b
jusbenny0 <stardestrojr@gmail.com>
2025-01-25 22:26:37 +0100

    fixed google oauth

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,7 @@
 import express from 'express';
-import { signup, login, google, signout } from '../controllers/auth.controller.js';
+import { signup, login, signout } from '../controllers/auth.controller.js';
 
 const router = express.Router()
 
-router.post('/signup', signup)
-router.post('/login', login)
-router.post('/google', google)
-router.get('/signout', signout);
 
 export default router
